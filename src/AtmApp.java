5dcc8addb69c094d9b698b258ca144ca18ceb8a5
Hamza Doğan <hamzadogan85@gmail.com>
2023-02-22 21:32:18 +0300

    v4

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -4,6 +4,8 @@ public class AtmApp {
         //version 2
         System.out.println("Java is easy");
         System.out.println("Learn Java Earn Money");
+        //version4
+        System.out.println("Giti öğrenmek çok kolay");
 
     }
 }
